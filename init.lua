aef33655f8b540900a269ecac7bda33d32189c49
soulsam480 <soulsam480@hotmail.com>
2024-10-06 01:04:13 +0530

    feat: init

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("config.lazy")
+
+vim.cmd[[colorscheme tokyonight-night]]
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
