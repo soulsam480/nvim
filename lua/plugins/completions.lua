f68d46d6094d464026c040a32f182d27cebc2d33
Sambit Sahoo <soulsam480@hotmail.com>
2024-11-05 13:41:37 +0530

    feat: upgrade blink and fix keymaps

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -3,23 +3,34 @@ return {
 		"saghen/blink.cmp",
 		event = { "BufReadPre", "BufNewFile" },
 		dependencies = "rafamadriz/friendly-snippets",
-		version = "v0.4.1",
+		version = "v0.*",
 		config = function()
 			require("blink.cmp").setup({
 				keymap = {
-					show = "<C-space>",
-					hide = "<C-e>",
-					accept = "<Tab>",
-					select_prev = { "<Up>", "<C-n>" },
-					select_next = { "<Down>", "<C-p>" },
+					-- show = "<C-space>",
+					-- hide = "<C-e>",
+					-- accept = "<Tab>",
+					-- select_prev = { "<Up>", "<C-n>" },
+					-- select_next = { "<Down>", "<C-p>" },
+					--
+					-- show_documentation = "<C-space>",
+					-- hide_documentation = "<C-space>",
+					-- scroll_documentation_up = "<C-b>",
+					-- scroll_documentation_down = "<C-f>",
+					--
+					-- snippet_forward = "<C-n>",
+					-- snippet_backward = "<C-p>",
+					preset = "default",
+					["<Tab>"] = { "select_and_accept" },
 
-					show_documentation = "<C-space>",
-					hide_documentation = "<C-space>",
-					scroll_documentation_up = "<C-b>",
-					scroll_documentation_down = "<C-f>",
+					["<Up>"] = { "select_prev", "fallback" },
+					["<Down>"] = { "select_next", "fallback" },
 
-					snippet_forward = "<C-n>",
-					snippet_backward = "<C-p>",
+					["<C-b>"] = { "scroll_documentation_up", "fallback" },
+					["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+					["<C-n>"] = { "snippet_forward", "fallback" },
+					["<C-p>"] = { "snippet_backward", "fallback" },
 				},
 				nerd_font_variant = "mono",
 				accept = { auto_brackets = { enabled = true } },
--- lua/plugins/signs.lua
+++ lua/plugins/signs.lua
@@ -84,7 +84,7 @@ return {
 		opts = {
 			enabled = true,
 			message_template = " <summary> • <date> • <author> • <<sha>>",
-			date_format = "%m-%d-%Y %H:%M:%S",
+			date_format = "%r %m-%d-%Y %H:%M:%S",
 			virtual_text_column = 1,
 		},
 	},
