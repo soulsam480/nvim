ca4d9c6fd8f00f2d1018c92f9c0a06c56c0fdce4
Sambit Sahoo <soulsam480@hotmail.com>
2024-11-12 10:48:21 +0530

    feat: rememver previous buffers and splits

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -209,4 +209,15 @@ return {
 			})
 		end,
 	},
+	{
+		"rmagatti/auto-session",
+		lazy = false,
+		---enables autocomplete for opts
+		---@module "auto-session"
+		---@type AutoSession.Config
+		opts = {
+			suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+			-- log_level = 'debug',
+		},
+	},
 }
