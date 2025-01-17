433fc0bbb53d16a351ef0ace75ce314787e4196f
Sambit Sahoo <soulsam480@hotmail.com>
2025-01-17 23:26:55 +0530

    feat: added user command to copy file

--- lua/config/usercmds.lua
+++ lua/config/usercmds.lua
@@ -3,3 +3,14 @@ vim.api.nvim_create_user_command("CopyRelPath", function()
 end, {
 	desc = "Copy relative path of active buffer",
 })
+
+
+vim.api.nvim_create_user_command("CopyFile", function()
+		local file_path = vim.fn.expand("%:p")
+		local file_contents = vim.fn.readfile(file_path)
+		local contents = table.concat(file_contents, "\n")
+
+		vim.api.nvim_call_function("setreg", { "+", contents })
+	end,
+	{ desc = "Copy contents of current buffer to clipboard" }
+)
