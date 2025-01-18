6277bd2a8ae80e4347edb44bf27a06970beb8340
Sambit Sahoo <soulsam480@hotmail.com>
2025-01-18 11:11:00 +0530

    fix: make copy to clipboard work

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -1,12 +1,11 @@
--- Copy to clipboard
-vim.cmd([[
-  nnoremap <leader>y "+y
-  nnoremap <leader>yy "+yy
-]])
+vim.keymap.set({ "n", "v", "^V" }, "<leader>y", '"+y', { noremap = true, desc = "Copy to clipboard" })
+
+vim.keymap.set({ "n", "v", "^V" }, "<leader>yy", '"+yy', { noremap = true, desc = "Copy line to clipboard" })
 
 vim.keymap.set("n", "<C-c>", "<cmd>:bdelete<CR>", { desc = "Close current buffer" })
 
 vim.keymap.set("n", "<leader>cc", "<cmd>%bd<CR>", { desc = "Close all open buffers" })
 
 vim.keymap.set("n", "<C-u>", "15k", { noremap = true })
+
 vim.keymap.set("n", "<C-d>", "15j", { noremap = true })
